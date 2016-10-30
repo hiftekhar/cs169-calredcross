72a958a930da24ed39d6cea4900f3c9eb1761e58
Bozhao Jin <bozhaojin@berkeley.edu>
2016-10-30 01:37:23 +0000

    seeded database with a dummy officer account

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# User.create!([{
-#   email: "sampleofficer@gmail.com",
-#   sign_in_count: 0,
-#   name: "sample_officer",
-  
-  
-#   director: "Peyton Reed",
-#   storyline: "Armed with the astonishing ability to shrink in scale but increase in strength, con-man Scott Lang must embrace his inner-hero and help his mentor, Dr. Hank Pym, protect the secret behind his spectacular Ant-Man suit from a new generation of towering threats. Against seemingly insurmountable obstacles, Pym and Lang must plan and pull off a heist that will save the world.",
-#   watched_on: 5.days.ago
-# },])
+User.create!([{
+  email: "sampleofficer@gmail.com",
+  sign_in_count: 0,
+  name: "sample_officer",
+  is_officer: 1,
+  password: "Foobar123!",
+  num_hours: 30,
+  status: "active",
+  medal: "gold"
+},])
 
-
-# t.string   "email",                  default: "", null: false
-#     t.string   "encrypted_password",     default: "", null: false
-#     t.string   "reset_password_token"
-#     t.datetime "reset_password_sent_at"
-#     t.datetime "remember_created_at"
-#     t.integer  "sign_in_count",          default: 0,  null: false
-#     t.datetime "current_sign_in_at"
-#     t.datetime "last_sign_in_at"
-#     t.string   "current_sign_in_ip"
-#     t.string   "last_sign_in_ip"
-#     t.datetime "created_at",                          null: false
-#     t.datetime "updated_at",                          null: false
-#     t.string   "major"
-#     t.string   "name"
-#     t.integer  "is_officer"
-#     t.index ["email"], name: "index_users_on_email", unique: true
-#     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+p "Created #{User.count} users"
