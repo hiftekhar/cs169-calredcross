e664d80285e9589950ac843ca009e91ff3b07107
Huda Iftekhar <hiftekhar@berkeley.edu>
2016-11-01 21:34:08 +0000

    Authentication Attempt

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 source 'https://rubygems.org'
 
+gem 'omniauth'
+gem 'omniauth-google-oauth2'
+gem 'google_drive'
+gem 'google-api-client', '0.8.6', require: 'google/api_client' 
+
 
 gem 'fullcalendar-rails'
 gem 'momentjs-rails'
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,5 +1,15 @@
+#require 'google/apis/drive_v2'
+require 'google/api_client/client_secrets'
+
+
 class ProfileController < ApplicationController
   def index
     @user = current_user
+    client_secrets = Google::APIClient::ClientSecrets.load
+    auth_client = client_secrets.to_authorization
+    auth_client.update!(
+      :scope => 'https://www.googleapis.com/auth/calendar',
+      :redirect_uri => 'http://www.example.com/oauth2callback'
+    )
   end
 end
