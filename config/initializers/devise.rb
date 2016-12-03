9ba7f5ef4a2f36995eba446dff7f30e16fdf63c2
Huda Iftekhar <hiftekhar@berkeley.edu>
2016-12-03 06:46:10 +0000

    try heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,6 +269,11 @@ Devise.setup do |config|
   # The router that invoked `devise_for`, in the example above, would be:
   # config.router_name = :my_engine
   #
+  
+  config.omniauth :google_oauth2, Settings.ga.app_key,Settings.ga.app_secret,{ 
+  access_type: "offline", 
+  approval_prompt: "" ,
+  :scope => "userinfo.email, userinfo.profile, plus.me, analytics.readonly"}
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
